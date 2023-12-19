5a566bfb34e519f14cee7025efaa926fc2aca623
hcn31 <saidihocuine913@gmail.com>
2023-12-19 12:32:03 +0100

    prblm resolved by changing versu=ion of deoendencies

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,25 +1,33 @@
 import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
+import { MutationCache } from '@tanstack/react-query'
+
 
 const Page = async () => {
-const router = useRouter()
 
-const searchParams=useSearchParams()
-const origin=searchParams.get('origin')
-trpc.authCallback.useQuery(undefined, {
+
+  const mutationCache = new MutationCache({
+    onError: (error:any) => {
+      if (error.data?.code === 'UNAUTHORIZED') {
+        router.push('/sign-in')
+      }
+    },
     onSuccess:({ success }:any) => {
       if (success) {
         // user is synced to db
         router.push(origin ? `/${origin}` : '/dashboard')
       }
     },
-    onError: (err:any) => {
-      if (err.data?.code === 'UNAUTHORIZED') {
-        router.push('/sign-in')
-      }
-    },
-    retry: true,
-    retryDelay: 500,
+    
+  })
+
+const router = useRouter()
+
+const searchParams=useSearchParams()
+const origin=searchParams.get('origin')
+
+trpc.authCallback.useQuery(undefined, {
+  mutationCache,
   })
 
 }
