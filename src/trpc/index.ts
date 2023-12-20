cdac650903e785da592d93fdea33d8c35aed7d85
BoumouzounaBrahimVall <brahimvall.boumouzouna@etu.fstm.ac.ma>
2023-12-20 01:10:22 +0100

    showing file list done - deleting file done

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -2,6 +2,7 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { privateProcedure, publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
 import { db } from '@/db';
+import {z} from 'zod';
 //initialize our main router instance in which we'll later add procedures to (API logic)
 export const appRouter = router({  
   
@@ -23,7 +24,6 @@ export const appRouter = router({
     })
 
     if (!dbUser) {
-      // create user in db  haaaaaaaaaaaadi khassha tkhdem
       await db.user.create({
         data: {
           id: user.id,
@@ -35,7 +35,6 @@ export const appRouter = router({
 
         return {success:true}
   }),
-  //hadiiii makatrje3ch l files
   getUserFiles: privateProcedure.query(async ({ ctx }) => {
     const { userId } = ctx
     return await db.file.findMany({
@@ -43,6 +42,25 @@ export const appRouter = router({
         userId,
       }
     })
+  }),
+  deleteFile: privateProcedure.input(
+    z.object({id: z.string()})
+
+  ).mutation(async ({ctx,input})=> {
+    const { userId } = ctx
+    const file = await db.file.findFirst({
+      where: {
+        id: input.id,
+        userId,
+      }
+    })
+    if(!file ) throw new TRPCError({code: 'NOT_FOUND'})
+    await db.file.delete({
+      where:{
+        id: input.id,
+      }
+    })
+    return file;
   }),
 
 });
